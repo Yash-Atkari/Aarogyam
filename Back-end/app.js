ad560a3a9afcb231f541ca212065bfaf5024f38f
Yash Atkari <yashatkari7@gmail.com>
2025-02-19 23:01:08 +0530

    Create Dashboard Page

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/admin/dashboard', (req, res) => {
-  res.render('admin/dashboard');
+app.get("/aarogyam", (req, res) => {
+  res.render("dashboard");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get("/admin/dashboard", (req, res) => {
+  res.render("admin/dashboard");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
