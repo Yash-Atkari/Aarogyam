ed32360849cea64c4534be768d64d6c32824f034
Yash Atkari <yashatkari7@gmail.com>
2025-02-28 15:30:14 +0530

    Appointment page created

--- app.js
+++ app.js
@@ -64,24 +64,63 @@ app.get("/patient/signup", (req, res) => {
 
 app.get("/patient/dashboard", async (req, res) => {
   try {
-    req.user = "67b6d14db339e23694c73bf9";
+    const patientId = "67b6d14db339e23694c73bf9";
+    const patient = await Patient.findById(patientId);
 
-    const healthrecords = await HealthRecord.find({patientId: req.user});
-    const doctors = await Doctor.find({});
-    const appointments = await Appointment.find({ patientId: req.user});
+    if (!patient) return res.status(404).json({ error: "Patient not found" });
 
-    // console.log(healthrecords);
-    // console.log(doctors);
-    // console.log(appointments);
-
-    
+    res.render("patient/dashboard", { patient });
   } catch (err) {
     console.error("Error fetching patient data:", err);
     res.status(500).json({ error: "Internal Server Error", details: err.message });
   }
-  res.render("patient/dashboard");
 });
 
+app.get("/patient/appointments", async (req, res) => {
+  try {
+    const patientId = "67b6d14db339e23694c73bf9";
+    const appointments = await Appointment.find({patientId: patientId})
+    .populate("patientId")  // Populating patient details
+    .populate("doctorId");  // Populating doctor details
+
+    res.render("patient/appointments/todaysappointments", { appointments });
+  } catch (err) {
+    console.error("Error fetching appointments:", err);
+    res.status(500).json({ error: "Internal Server Error", details: err.message });
+  }
+});
+
+app.get("/patient/bookappointment", (req, res) => {
+  try {
+    res.render("patient/appointments/bookappointment");
+  } catch (err) {
+    console.error("Error rendering the appointment booking page:", err);
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+
+// app.get("/patient/healthrecords", async (req, res) => {
+//   try {
+//     const patientId = "67b6d14db339e23694c73bf9";
+//     const healthrecords = await HealthRecord.find({patientId: patientId});
+
+//     res.render("patient/healthrecords", { healthrecords });
+//   } catch (err) {
+//     console.error("Error fetching health records:", err);
+//     res.status(500).json({ error: "Internal Server Error", details: err.message });
+//   }
+// });
+
+// app.get("/patient/doctors", async (req, res) => {
+//   try {
+//     const doctors = await Doctor.find();
+//     res.render("patient/doctors", { doctors });
+//   } catch (err) {
+//     console.error("Error fetching doctors:", err);
+//     res.status(500).json({ error: "Internal Server Error", details: err.message });
+//   }
+// });
+
 app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000/aarogyam`);
+  console.log("Server is running on http://localhost:3000");
 });
