f0c361fe9eb5dabb31694bebd052f2145fcea38a
Yash Atkari <yashatkari7@gmail.com>
2025-03-06 21:31:20 +0530

    x

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 const bodyParser = require("body-parser");
 
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 
